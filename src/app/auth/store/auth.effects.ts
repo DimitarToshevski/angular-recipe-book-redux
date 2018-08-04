d4b221b7909a554cae5f3ae6fc3555ce6f69dc4e
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-08-04 17:45:07 +0300

    dispatching actions when signup through the store, added TRY_SIGNUP action to dispatch when signup attend is made, listening to TRY_SIGNUP in authEffects, consuming the request to FIrebase as a side Effect through the store, triggering the SIGNUP and SET_TOKEN inside the Effect, added comments

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,37 @@
 import { Effect, Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 
+import * as AuthAction from './auth.actions';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+
+import * as firebase from 'firebase';
+import { from } from 'rxjs';
+
 @Injectable()
 export class AuthEffects {
   @Effect() // it marks the property below and will execute the code on the right of it
-  authSignup;
+  authSignup = this.actions$ // checking all of the actions provided by all of the rxjs stores in our app
+    .ofType(AuthAction.TRY_SIGNUP) // if anywhere in our app a TRY_SIGNUP action is triggered the code below will execute
+    .pipe( map( (action: AuthAction.TrySignup) => { // getting just the payload of the triggered action from the observable
+      return action.payload;
+    }))
+    .pipe( switchMap( ( authData: { username: string, password: string } ) => {
+      // using switchMap so inside the passed observable, we invoke a function which also returns an observable
+      return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+      // passing down the observable that the inner function returns,  by switchng it with the outer observable
+    }))
+    .pipe( switchMap( () => { // again switching the outer observable with the observable from the function we call inside it
+      return from(firebase.auth().currentUser.getIdToken());
+    }))
+    .pipe( mergeMap( (token: string) => { // now switching the outer observable with multiple observable we want to return
+      return [
+        { type: AuthAction.SIGNUP },
+        {
+          type: AuthAction.SET_TOKEN,
+          payload: token
+        }
+      ];
+    }));
 
   constructor (private actions$: Actions) {} // propery marked with $ to say its an observable
 }
--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -17,6 +17,9 @@ export class AuthInterceptor implements HttpInterceptor {
     return this.store.select('auth')
     .pipe(take(1)) // take the value of selected state(auth) just once so if changes occur we dont send another req
     .pipe( switchMap( (authState: fromAuth.State ) => {
+      // the map will take the observable and wrap it in another observable
+      // the switchMap will get the observable and return new observable by switching the old one with the new one
+      // if we want to return multiple observables as one object we use mergeMap
 
       const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
       return next.handle(copiedReq);
